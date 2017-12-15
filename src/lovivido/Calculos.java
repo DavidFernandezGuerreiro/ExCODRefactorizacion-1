5635ca06057d01d88ec29b0a9ab660345f863eb4
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-12-15 08:52:01 +0100

    COMMIT GITHUB.

---
+++ src/lovivido/Calculos.java
@@ -0,0 +1,47 @@
+package lovivido;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Calculos {
+    String nombre;
+    String edad;
+
+    int meses;
+    int dias;
+    int horas;
+    int años;
+    
+    /**
+     * @param nombre Parámetro en el que agregamos el nombre.
+     * @param edad Parámetro en el que agregamos la edad.
+    */
+    
+    public void recibirNomEd(String nombre, String edad){
+        nombre=JOptionPane.showInputDialog("Escriba su nombre: ");
+        edad=JOptionPane.showInputDialog("Escriba su edad: ");
+        años=Integer.parseInt(edad);
+    }   
+    
+    /**
+     * @param meses Parámetro en el que muestra los meses vividos.
+     * @param dias Parámetro en el que muestra los dias vividos.
+     * @param horas Parámetro en el que muestra las horas vividas.
+     */
+    public void calcularLoVivido(int meses, int dias, int horas){
+        meses=(años * 12);
+        dias=(años * 365);
+        horas=(dias * 24);
+        
+        JOptionPane.showMessageDialog(null, "Meses: " + meses, "Numero de meses vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Días: " + dias, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Horas: " + horas, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+}
